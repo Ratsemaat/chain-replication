b496d0cd76f696a520631508682deda44733ce48
Herman Rull <rullherman@gmail.com>
2023-05-06 13:36:43 +0300

    reformat and get_ip as static func

--- classes/node.py
+++ classes/node.py
@@ -16,16 +16,23 @@ port_map = {
     3: 50053,
 }
 
-debug=True
-flows=["create_chain","all", "connection"]
+debug = True
+flows = ["create_chain", "all", "connection"]
+
+
 def debug(par, flow="all"):
     if debug and flow in flows:
         print(par)
 
-class Node (node_pb2_grpc.ChainReplicationService):
+
+def get_ip(ide):
+    return f"{network}:{port_map[ide]}"
+
+
+class Node(node_pb2_grpc.ChainReplicationService):
 
     def __init__(self, id):
-        self.idx=1
+        self.idx = 1
         self.id = id
         self.data_stores = []
         self.chain = None
@@ -40,7 +47,7 @@ class Node (node_pb2_grpc.ChainReplicationService):
 
     def GetDataStores(self, request, context):
         debug(str(self.data_stores), flow="create_chain")
-        if len(self.data_stores)==0:
+        if len(self.data_stores) == 0:
             return node_pb2.DataStoreList(data_store_ids=[])
         stores_as_string = str(self.data_stores)[1:][:-1]
         return node_pb2.DataStoreList(data_store_ids=stores_as_string.split(", "))
@@ -57,37 +64,34 @@ class Node (node_pb2_grpc.ChainReplicationService):
     def list_chain(self):
         print(repr(self.chain))
 
-
     def init_new_data_store(self):
         self.data_stores.append(DataStore(self.idx, self.id))
-        self.idx+=1
+        self.idx += 1
 
     def is_target_alive(self, target):
-        with grpc.insecure_channel(self.get_ip(target)) as channel:
+        with grpc.insecure_channel(get_ip(target)) as channel:
             try:
                 stub = node_pb2_grpc.ChainReplicationServiceStub(channel)
                 resp = stub.IsAliveCheck(node_pb2.Empty())
-                debug(f"Node with ip {self.get_ip(target)} is available, response {resp} ", flow="connection")
+                debug(f"Node with ip {get_ip(target)} is available, response {resp} ", flow="connection")
                 return resp and resp.alive
             except grpc._channel._InactiveRpcError as e:
-                debug(f"Node with ip {self.get_ip(target)} is unavailable ", flow="connection")
+                debug(f"Node with ip {get_ip(target)} is unavailable ", flow="connection")
                 return False
 
     def get_target_nodes_data_stores(self, target):
-        if target==self.id:
+        if target == self.id:
             debug(f"{self.data_stores}", flow="create_chain")
             return self.data_stores
-        with grpc.insecure_channel(self.get_ip(target)) as channel:
+        with grpc.insecure_channel(get_ip(target)) as channel:
             stub = node_pb2_grpc.ChainReplicationServiceStub(channel)
             resp = stub.GetDataStores(node_pb2.Empty())
             debug(f"{resp}", flow="create_chain")
 
             return resp.data_store_ids
 
-
-
     def get_active_nodes(self):
-        #TODO: Make  it parallel
+        # TODO: Make  it parallel
         active_nodes = []
         for k in port_map.keys():
             debug(k)
@@ -98,14 +102,12 @@ class Node (node_pb2_grpc.ChainReplicationService):
                 if self.is_target_alive(k):
                     active_nodes.append(k)
         return active_nodes
-    def get_ip(self, ide):
-        return f"{network}:{port_map[ide]}"
 
     def send_chain(self, target):
         if target == self.id:
             return True
 
-        with grpc.insecure_channel(self.get_ip(target)) as channel:
+        with grpc.insecure_channel(get_ip(target)) as channel:
             stub = node_pb2_grpc.ChainReplicationServiceStub(channel)
             debug(f"chain: {str(self.chain).split(',')}", flow="create_chain")
             resp = stub.TransferChain(node_pb2.Chain(chain=str(self.chain).split(",")))
@@ -124,30 +126,19 @@ class Node (node_pb2_grpc.ChainReplicationService):
         debug(f"stores: {stores}, length {len(stores)}", flow="create_chain")
 
         self.chain = Chain()
-        if len(stores)==0:
+        if len(stores) == 0:
             raise RuntimeError("Failed to create the chain. No datastores found.")
         self.chain.head = stores[0]
         self.chain.tail = stores[-1]
         self.chain.processes = stores
 
         all_accepted = True
-        #TODO: sent acknowledgement that about chain creation result
+        # TODO: sent acknowledgement that about chain creation result
         for node in nodes:
             all_accepted = all_accepted and self.send_chain(node)
 
         if not all_accepted:
-            self.chain=None
+            self.chain = None
             raise RuntimeError("Failed to create the chain")
 
         return
-
-
-
-
-
-
-
-
-
-
-
